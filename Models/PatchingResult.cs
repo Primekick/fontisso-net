d8d1e572f045cbfd0b2994d385bb0e9f57bd3d6e
Primekick <thetrueaxer@gmail.com>
2025-01-14 23:50:54 +0100

    Add dialog host

---
+++ Models/PatchingResult.cs
@@ -0,0 +1,7 @@
+﻿namespace Fontisso.NET.Models;
+
+public record PatchingResult(string Title, string Content)
+{
+    public static PatchingResult OkResult(string content) => new("Info", content);
+    public static PatchingResult ErrorResult(string content) => new("Błąd", content);
+}
