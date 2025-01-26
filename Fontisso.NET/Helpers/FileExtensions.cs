3d9a00c93cf2d9ab3888672e2b18f4086353b9b3
Primekick <thetrueaxer@gmail.com>
2025-01-26 19:35:26 +0100

    Add binary file writing extension

---
+++ Fontisso.NET/Helpers/FileExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Fontisso.NET.Helpers;
+
+public static class FileExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void OpenAndWrite(string path, ReadOnlySpan<byte> buffer)
+    {
+        using var writer = new BinaryWriter(File.Open(path, FileMode.Create));
+        writer.Write(buffer);
+    }
+}
