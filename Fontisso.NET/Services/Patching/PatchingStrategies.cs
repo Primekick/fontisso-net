4d1c762924c5ba9c78f9a83533c97184af7acdff
Primekick <thetrueaxer@gmail.com>
2025-01-26 20:10:26 +0100

    Add patching strategy implementations

--- Fontisso.NET/Services/Patching/PatchingStrategies.cs
+++ Fontisso.NET/Services/Patching/PatchingStrategies.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 using Fontisso.NET.Configuration.Patching;
+using Fontisso.NET.Data.Models;
+using Fontisso.NET.Helpers;
+using Fontisso.NET.Services.Metadata;
+using PeNet;
 
 namespace Fontisso.NET.Services.Patching;
 
@@ -8,11 +14,44 @@ public interface IPatchingStrategy
     void Patch(string filePath, ReadOnlyMemory<byte> rpg2000Data, ReadOnlyMemory<byte> rpg2000GData);
 }
 
-public sealed class LegacyPatchingStrategy(LegacyPatchingConfig config) : IPatchingStrategy
+public sealed class LegacyPatchingStrategy(LegacyPatchingConfig config, IFontMetadataProcessor fontMetadata)
+    : IPatchingStrategy
 {
     public void Patch(string filePath, ReadOnlyMemory<byte> rpg2000Data, ReadOnlyMemory<byte> rpg2000GData)
     {
-        throw new NotImplementedException();
+        var exeRootDir = Path.GetDirectoryName(filePath)!;
+        var currentDir = Directory.GetCurrentDirectory();
+        File.Copy(
+            Path.Combine(currentDir, config.LegacyLoaderDllName),
+            Path.Combine(exeRootDir, config.LegacyLoaderDllName),
+            true
+        );
+
+        var customFontDataA = fontMetadata.SetFaceName(rpg2000Data, config.CustomFontNameA.Span);
+        var customFontDataB = fontMetadata.SetFaceName(rpg2000GData, config.CustomFontNameB.Span);
+        Directory.CreateDirectory(Path.Combine(exeRootDir, config.FontsDirectory));
+        FileExtensions.OpenAndWrite(
+            Path.Combine(exeRootDir, config.FontsDirectory, config.FontFileNameA),
+            customFontDataA.Span
+        );
+        FileExtensions.OpenAndWrite(
+            Path.Combine(exeRootDir, config.FontsDirectory, config.FontFileNameB),
+            customFontDataB.Span
+        );
+
+        var peFile = new PeFile(filePath);
+        if (peFile.ImportedFunctions!.All(func => func.DLL != config.LegacyLoaderDllName))
+        {
+            peFile.AddImport(config.LegacyLoaderDllName, "Dummy");
+        }
+
+        var binary = peFile.RawFile.ToArray().AsSpan();
+        // sometimes the builtin font name appears twice in the game binary hence two replacement calls - needs more research
+        binary.Replace(config.BuiltinFontNameA.Span, config.CustomFontNameA.Span);
+        binary.Replace(config.BuiltinFontNameA.Span, config.CustomFontNameA.Span);
+        binary.Replace(config.BuiltinFontNameB.Span, config.CustomFontNameB.Span);
+        binary.Replace(config.BuiltinFontNameB.Span, config.CustomFontNameB.Span);
+        FileExtensions.OpenAndWrite(filePath, binary);
     }
 }
 
@@ -20,6 +59,7 @@ public sealed class ModernPatchingStrategy(IResourceService resourceService) : I
 {
     public void Patch(string filePath, ReadOnlyMemory<byte> rpg2000Data, ReadOnlyMemory<byte> rpg2000GData)
     {
-        throw new NotImplementedException();
+        var resources = new[] { (FontKind.RPG2000, rpg2000Data), (FontKind.RPG2000G, rpg2000GData) };
+        resourceService.WriteResources(filePath, resources);
     }
 }
