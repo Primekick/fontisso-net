d3b13424277e0108867a0d3d186c323479715665
Primekick <thetrueaxer@gmail.com>
2025-01-26 19:34:58 +0100

    Add span replacing extension

---
+++ Fontisso.NET/Helpers/SpanExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Fontisso.NET.Helpers;
+
+public static class SpanExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void Replace(this Span<byte> target, ReadOnlySpan<byte> oldValue, ReadOnlySpan<byte> newValue)
+    {
+        var index = target.IndexOf(oldValue);
+        if (index >= 0)
+        {
+            newValue.CopyTo(target.Slice(index, oldValue.Length));
+        }
+    }
+}
